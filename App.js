adc37919e0cbf67f990a2cfd39faadce3d992dae
g3un <g3un98@gmail.com>
2021-11-10 18:34:30 +0900

    Separate Screens

--- App.js
+++ App.js
@@ -1,40 +1,11 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-function CtfsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ctfs!</Text>
-    </View>
-  );
-}
-
-function MarkedScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Marked!</Text>
-    </View>
-  );
-}
-
-function SearchScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import CtfsScreen from './components/CtfsScreen';
+import MarkedScreen from './components/MarkedScreen';
+import SearchScreen from './components/SearchScreen';
+import SettingsScreen from './components/SettingsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -42,8 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
             let iconName;
 
             if (route.name === 'Ctfs') {
@@ -56,17 +27,15 @@ export default function App() {
               iconName = focused ? 'settings' : 'settings-outline';
             }
 
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: 'orange',
           tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name='Ctfs' component={CtfsScreen} />
-        <Tab.Screen name='Marked' component={MarkedScreen} />
-        <Tab.Screen name='Search' component={SearchScreen} />
-        <Tab.Screen name='Settings' component={SettingsScreen} />
+        })}>
+        <Tab.Screen name="Ctfs" component={CtfsScreen} />
+        <Tab.Screen name="Marked" component={MarkedScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
