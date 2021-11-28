266dae59572e3ce63ef4f1c17a6609fdc68fe2fe
N0N4M3D-b <20wlsaud@gmail.com>
2021-11-28 23:55:46 +0900

    Add fetch for get api info

---
+++ api/fetch.js
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+import { InsertCtfDb } from "../database/CtfDb";
+
+export const FetchCtfInfo = async (startTime, finishTime) => { 
+  console.log("[FetchCtfInfo] Fetch && DB Insert Test Start");
+  const GetJsonData = url => {
+    return fetch(url)
+      .then(res => res.json());
+  }
+
+  let Url = `https://ctftime.org/api/v1/events/?limit=500&start=${startTime}&finish=${finishTime}`;
+
+  let ctfInfoJsonList = await GetJsonData(Url);
+  // console.log(ctfInfoJsonList);
+  InsertCtfDb(ctfInfoJsonList);
+}
+
