0e6eefcacaad55f252a02ca58c4d282996c4de5e
N0N4M3D-b <20wlsaud@gmail.com>
2021-11-30 18:24:53 +0900

    Fetch fork repo

--- api/fetch.js
+++ api/fetch.js
@@ -8,7 +8,7 @@ export const fetchCtfInfo = async (startTime, finishTime) => {
       .then(res => res.json());
   }
 
-  let URL = `https://ctftime.org/api/v1/events/?limit=5&start=${startTime}&finish=${finishTime}`;
+  let URL = `https://ctftime.org/api/v1/events/?limit=500&start=${startTime}&finish=${finishTime}`;
 
   let ctfInfoJsonList = await getJsonData(URL);
   // console.log(ctfInfoJsonList);
