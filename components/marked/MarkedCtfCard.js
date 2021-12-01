95e91cc946ecf1ed8037edcd91b486a8380e94a1
N0N4M3D-b <20wlsaud@gmail.com>
2021-12-01 22:30:54 +0900

    Add component controller

--- components/marked/MarkedCtfCard.js
+++ components/marked/MarkedCtfCard.js
@@ -3,20 +3,7 @@ import { Button, Text, View } from "react-native";
 
 // const MarkedCtfCard = ({ title, navigation, id, markedCtfs, deleteMarkedCtfs }) => {
 const MarkedCtfCard = (props) => {
-  const deleteMarkedCtf = (id) => {
-    /*
-    let tmpMarkedCtfs = markedCtfs;
-    const deleteIndex = tmpMarkedCtfs.findIndex(function(item) {
-      return item.id === id;
-    });
-
-    console.log("Delete ID: " + tmpMarkedCtfs[deleteIndex].id);
-    tmpMarkedCtfs.splice(deleteIndex, 1);
-    // database delete function add
-    deleteMarkedCtfs(tmpMarkedCtfs);
-    */
-    props.deleteMarkedCtfs(id);
-  };
+  const deleteMarkedCtfList = (id) => {props.deleteMarkedCtfList(id)};
 
   return (
     <View
@@ -37,7 +24,7 @@ const MarkedCtfCard = (props) => {
       <Button
         title="Delete"
         onPress={() => {
-          deleteMarkedCtf(props.id);
+          deleteMarkedCtfList(props.id);
         }}
       />
     </View>
--- components/marked/MarkedCtfList.js
+++ components/marked/MarkedCtfList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useReducer } from "react";
 import { Text, ScrollView } from "react-native";
+import { MarkedListControl } from "../../src/componentState";
 import MarkedCtfCard from "./MarkedCtfCard";
 
 // IMPORT FOR API TEST
@@ -9,37 +10,21 @@ let initFlag = false;
 
 const MarkedCtfList = ({ navigation }) => {
   const [markedCtfs, updateMarkedCtfs] = useState([]);
-  const [_, forceUpdate] = useReducer((x) => x + 1, 0);
 
-  const deleteMarkedCtfList = (target_id) => {
-    console.log(`Delete ID: ${target_id}`);
-
-    // database delete function add
-    updateMarkedCtfs(markedCtfs.filter(({ id }) => id != target_id));
-    forceUpdate();
-  };
-
-  const updateMarkedCtfList = async (startTime) => {
-    // call database select function -> ctfs = select(???)
-    const newMarkedCtfList = await fetchCtfInfo((startTime = startTime));
-    updateMarkedCtfs(newMarkedCtfList);
-  };
+  const deleteMarkedCtfList = (id) => {updateMarkedCtfs(MarkedListControl.deleteMarkedCtfList(id, markedCtfs))};
+  const updateMarkedCtfList = async (startTime, finishTime) => {updateMarkedCtfs(await MarkedListControl.updateMarkedCtfList(startTime, finishTime))};
 
   if (!initFlag) {
     initFlag = true;
     updateMarkedCtfList();
   }
-
-  useEffect(() => {
-    console.log("Render complete");
-  });
   return (
     <ScrollView>
       {markedCtfs.map((markedCtf) => (
         <MarkedCtfCard
           key={markedCtf.id}
           markedCtfs={markedCtfs}
-          deleteMarkedCtfs={deleteMarkedCtfList}
+          deleteMarkedCtfList={deleteMarkedCtfList}
           {...markedCtf}
           navigation={navigation}
         />
