c53703f67b15ee1f66d35a285aed52d389441592
hosecloud <clftjdtmxk@naver.com>
2021-12-04 14:19:13 +0900

    db select, delete finish

--- database/CtfDb.js
+++ database/CtfDb.js
@@ -77,7 +77,7 @@ const _dropEventTable = () => {
   });
 };
 
-const _dropOrganiverTable = () => {
+const _dropOrganizerTable = () => {
   _ctf_db.transaction((txn) => {
     txn.executeSql(
       "DROP TABLE organizer",
@@ -98,7 +98,7 @@ const _insertOrganizerTable = (organizers) => {
       txn.executeSql(
         `INSERT INTO organizer VALUES(
           ${organizer.id},
-          "${organizer.name.replaceall('"', '\\"')}"
+          "${organizer.name.replace(/"/g, '\\"')}"
         )`,
         [],
         (_, res) => {
@@ -122,22 +122,22 @@ const _insertEventTable = (event) => {
       `INSERT INTO event VALUES(
           ${event.organizers[0].id},
           ${event.onsite ? 1 : 0},
-          "${event.finish.replaceall('"', '\\"')}",
-          "${event.description.replaceall('"', '\\"')}",
+          "${event.finish.replace(/"/g, '\\"')}",
+          "${event.description.replace(/"/g, '\\"')}",
           ${event.weight},
-          "${event.title.replaceall('"', '\\"')}",
-          "${event.url.replaceall('"', '\\"')}",
+          "${event.title.replace(/"/g, '\\"')}",
+          "${event.url.replace(/"/g, '\\"')}",
           ${event.is_votable_now ? 1 : 0},
-          "${event.restrictions.replaceall('"', '\\"')}",
-          "${event.format.replaceall('"', '\\"')}",
-          "${event.start.replaceall('"', '\\"')}",
+          "${event.restrictions.replace(/"/g, '\\"')}",
+          "${event.format.replace(/"/g, '\\"')}",
+          "${event.start.replace(/"/g, '\\"')}",
           ${event.participants},
-          "${event.ctftime_url.replaceall('"', '\\"')}",
-          "${event.location.replaceall('"', '\\"')}",
-          "${event.live_feed.replaceall('"', '\\"')}",
+          "${event.ctftime_url.replace(/"/g, '\\"')}",
+          "${event.location.replace(/"/g, '\\"')}",
+          "${event.live_feed.replace(/"/g, '\\"')}",
           ${event.public_votable ? 1 : 0},
           ${duration},
-          "${event.logo.replaceall('"', '\\"')}",
+          "${event.logo.replace(/"/g, '\\"')}",
           ${event.format_id},
           ${event.id},
           ${event.ctf_id}
@@ -148,6 +148,87 @@ const _insertEventTable = (event) => {
       },
       (error) => {
         console.log(`insert event data failed: ${error.message}`);
+      },
+    );
+  });
+};
+
+const _selectOrganizerTable = () => {
+  let results = [];
+
+  _ctf_db.transaction((txn) => {
+    txn.executeSql(
+      "SELECT * FROM organizer",
+      [],
+      (_, res) => {
+        console.log("select organizer table successfully");
+        let len = res.rows.length;
+
+        if(len > 0) {
+          for(let i = 0; i < len; i++) {
+            let item = res.rows.item(i);
+            results.push(item);
+          }
+        }
+      },
+      (error) => {
+        console.log(`select organizer table failed: ${error.message}`);
+      },
+    );
+  });
+  return results;
+};
+
+const _selectEventTable = () => {
+  let results = [];
+  _ctf_db.transaction((txn) => {
+    txn.executeSql(
+      "SELECT * FROM event",
+      [],
+      (_, res) => {
+        console.log("select event table successfully");
+        let len = res.rows.length;
+
+        if(len > 0) {
+          for(let i = 0; i < len; i++) {
+            let item = res.rows.item(i);
+            results.push(item);
+          }
+        }
+      },
+      (error) => {
+        console.log(`select event table failed: ${error.message}`);
+      },
+    );
+  });
+  return results;
+};
+
+const _deleteOrganizerTable = (oid) => {
+  _ctf_db.transaction((txn) => {
+    txn.executeSql(
+      `DELETE FROM organizer WHERE id=${oid}`,
+      [],
+      (_, res) => {
+        console.log("delete organizer table successfully");
+      },
+      (error) => {
+        console.log(`delete organizer table failed: ${error.message}`);
+      },
+    );
+  });
+};
+
+const _deleteEventTable = (oid) => {
+  _ctf_db.transaction((txn) => {
+    txn.executeSql(
+      `DELETE FROM event WHERE id=${oid}`,
+      [],
+      (_, res) => {
+        console.log("delete event table successfully");
+      },
+      (error) => {
+        console.log(`delete event table failed: ${error.message}`);
       },
     );
   });
@@ -170,5 +251,17 @@ export const createCtfDb = () => {
 // Drop "event", "organizer" tables
 export const dropCtfDb = () => {
   _dropEventTable();
-  _dropOrganiverTable();
+  _dropOrganizerTable();
+};
+
+// Select "event", "organizer" tables
+export const selectCtfDb = () => {
+  return _selectEventTable();
+  
+};
+
+// Delete "event", "organizer" tables
+export const deleteCtfDb = () => {
+  _deleteEventTable();
+  _deleteOrganizerTable();
 };
